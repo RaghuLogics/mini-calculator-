77a6c764f582316ee290e94ca49ec14a2476a401
RaghuLogics <chundruraghuram@gmail.com>
2025-02-23 14:18:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+function AddNum(){
+    let num1 = document.getElementById('fristname').value
+    let num2 = document.getElementById('secondname').value
+    num1 = parseInt(num1)
+    num2 = parseInt(num2)
+    let h1 = document.getElementById('result')
+    res = num1+num2
+    h1.innerHTML="Result:" + res
+}
+function SubNum(){
+    let num1 = document.getElementById('fristname').value
+    let num2 = document.getElementById('secondname').value
+    num1 = parseInt(num1)
+    num2 = parseInt(num2)
+    let h1 = document.getElementById('result')
+    res = num1-num2
+    h1.innerHTML="Result:" + res
+
+}
+function MunNum(){
+    let num1 = document.getElementById('fristname').value
+    let num2 = document.getElementById('secondname').value
+    num1 = parseInt(num1)
+    num2 = parseInt(num2)
+    let h1=document.getElementById('result')
+    res= num1*num2
+    h1.innerHTML="Result:" + res
+}
+function DivNum(){
+    let num1 = document.getElementById('fristname').value
+    let num2 = document.getElementById('secondname').value
+    num1 = parseInt(num1)
+    num2 = parseInt(num2)
+    let h1=document.getElementById('result')
+    res = num1/num2
+    h1.innerHTML="Result:" + res
+}
+function PowNum(){
+    let num1 = document.getElementById('fristname').value
+    let num2 = document.getElementById('secondname').value
+    num1 = parseInt(num1)
+    num2 = parseInt(num2)
+    let h1=document.getElementById('result')
+    res = num1**num2
+    h1.innerHTML="Result:" + res
+}
